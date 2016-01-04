6e670f200eabc0eb3ae8a6455288927f90d7a191
Knightingal <yujianming1985@126.com>
2016-01-04 22:23:56 +0800

    implement startDonwloadTask,add getHttpReqCallback
    Changes to be committed:
          modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 (function() {
   "use strict";
+  const http = require("http");
+
   const urlList = [
     "http://content.battlenet.com.cn/wow/media/wallpapers/patch/fall-of-the-lich-king/fall-of-the-lich-king-1920x1080.jpg",
     "http://content.battlenet.com.cn/wow/media/wallpapers/patch/black-temple/black-temple-1920x1200.jpg",
@@ -8,8 +10,18 @@
     "http://content.battlenet.com.cn/wow/media/wallpapers/patch/fury-of-hellfire/fury-of-hellfire-3840x2160.jpg",
   ];
 
+  function getHttpReqCallback(imgSrc, dirName, index) {
+    var callback = function(res) {
+      
+    };
+
+    return callback;
+  }
+
   var startDownloadTask = function(imgSrc, dirName, index) {
-    //TODO: startDownloadTask
+    var req = http.request(imgSrc, getHttpReqCallback(imgSrc, dirName, index));
+    req.on('error', function(e){});
+    req.end();
   }
 
   urlList.forEach(function(item, index, array) {
