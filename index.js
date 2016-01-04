b8f1e232568d93368ac56644a3710adefb2a09b2
Knightingal <yujianming1985@126.com>
2016-01-04 21:52:21 +0800

    first commit, add urlList
    Changes to be committed:
          new file:   index.js
          new file:   package.json

---
+++ index.js
@@ -0,0 +1,10 @@
+(function() {
+  "use strict";
+  const urlList = [
+    "http://content.battlenet.com.cn/wow/media/wallpapers/patch/fall-of-the-lich-king/fall-of-the-lich-king-1920x1080.jpg",
+    "http://content.battlenet.com.cn/wow/media/wallpapers/patch/black-temple/black-temple-1920x1200.jpg",
+    "http://content.battlenet.com.cn/wow/media/wallpapers/patch/zandalari/zandalari-1920x1200.jpg",
+    "http://content.battlenet.com.cn/wow/media/wallpapers/patch/rage-of-the-firelands/rage-of-the-firelands-1920x1200.jpg",
+    "http://content.battlenet.com.cn/wow/media/wallpapers/patch/fury-of-hellfire/fury-of-hellfire-3840x2160.jpg",
+  ];
+})();
